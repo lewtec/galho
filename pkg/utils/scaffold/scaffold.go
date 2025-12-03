2b74370177966932129f1deeb4d9757013947d3b
lucasew <lucas59356@gmail.com>
2025-12-03 14:08:03 -0300

    checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- pkg/utils/scaffold/scaffold.go
+++ pkg/utils/scaffold/scaffold.go
@@ -4,5 +4,6 @@ import "io/fs"
 
 func InstallFS(destination string, data fs.FS) error {
 	// implement copy data from fs.FS to destination
+	// if extension ends with tmpl, copy without the tmpl extension
 	return nil
 }
