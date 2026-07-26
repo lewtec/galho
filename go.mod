0d6f89ec7435ac437ab32b9b4e0774ed2ba1ca9d
lucasew <lucas59356@gmail.com>
2026-07-26 18:14:15 +0000

    refactor(core): drop findup for stdlib project root walk
    findup ignores Getwd errors and joins paths with path (not filepath).
    Walk up from cwd with filepath/os.Stat and drop the dependency.

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/lewtec/galho
 go 1.24.3
 
 require (
-	github.com/h2non/findup v1.0.0
 	github.com/pelletier/go-toml/v2 v2.2.4
 	github.com/spf13/cobra v1.10.1
 )
--- pkg/core/project.go
+++ pkg/core/project.go
@@ -3,9 +3,9 @@ package core
 import (
 	"errors"
 	"fmt"
+	"io/fs"
+	"os"
 	"path/filepath"
-
-	"github.com/h2non/findup"
 )
 
 // ErrNotAGalhoProject is returned when GetProject cannot find a .galho marker
@@ -19,13 +19,24 @@ type Project struct {
 // GetProject walks up from the process working directory looking for a .galho
 // marker and returns the project rooted at that directory.
 func GetProject() (*Project, error) {
-	dotgalho, err := findup.Find(".galho")
+	dir, err := os.Getwd()
 	if err != nil {
-		return nil, fmt.Errorf("%w: no .galho marker found from cwd: %w", ErrNotAGalhoProject, err)
+		return nil, fmt.Errorf("%w: get working directory: %w", ErrNotAGalhoProject, err)
 	}
-	// findup returns a filesystem path; use filepath (not path) so Dir is correct
-	// on platforms where the separator is not '/'.
-	return &Project{dir: filepath.Dir(dotgalho)}, nil
+	start := dir
+	for {
+		marker := filepath.Join(dir, ".galho")
+		if _, err := os.Stat(marker); err == nil {
+			return &Project{dir: dir}, nil
+		} else if !errors.Is(err, fs.ErrNotExist) {
+			return nil, fmt.Errorf("%w: stat %s: %w", ErrNotAGalhoProject, marker, err)
+		}
+		parent := filepath.Dir(dir)
+		if parent == dir {
+			return nil, fmt.Errorf("%w: no .galho marker found from cwd %s", ErrNotAGalhoProject, start)
+		}
+		dir = parent
+	}
 }
 
 func (p *Project) Dir() string {
